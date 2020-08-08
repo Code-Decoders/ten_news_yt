dd635a5c657403413c8ff6f3147096d848fd6f3a
Maadhav <maadhav2001@gmail.com>
2020-08-08 20:37:17 +0530

    PART2 initial commit

--- lib/home.dart
+++ lib/home.dart
@@ -10,19 +10,168 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int currentIndex = 0;
 
-  void changePage(int index){
+  void changePage(int index) {
     setState(() {
       currentIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Builder(
+            builder: (context) => IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/drawer.svg",
+                height: 15,
+                width: 34,
+              ),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            ),
+          ),
+        ),
+        backgroundColor: currentIndex == 3 ? Color(0xffF7F8FA) : Colors.white,
+        elevation: 0,
+        centerTitle: false,
+        titleSpacing: 0,
+      ),
+      drawer: Container(
+        width: MediaQuery.of(context).size.width / 1.25,
+        child: Drawer(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 30,
+              ),
+              DrawerHeader(
+                child: Container(
+                    height: 142,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
+                      "assets/images/ten_news.png",
+                    )),
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    currentIndex = 3;
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontFamily: 'Avenir',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  'Settings',
+                  style: TextStyle(
+                    fontFamily: 'Avenir',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Text(
+                'About',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Text(
+                'Log Out',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(500),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(500),
+                  splashColor: Colors.black45,
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.black,
+                    child: Icon(Icons.arrow_back, color: Colors.white),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 65,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.black,
+                  child: Center(
+                    child: Text(
+                      'v1.0.1',
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 20,
+                        color: const Color(0xffffffff),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ))
+            ],
+          ),
+        ),
+      ),
       body: <Widget>[
-        Container(color: Colors.blue,),
-        Container(color: Colors.red,),
-        Container(color: Colors.yellow,),
-        Container(color: Colors.green,),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.green,
+        ),
       ][currentIndex],
       bottomNavigationBar: BubbleBottomBar(
         opacity: 0,
@@ -31,23 +180,68 @@ class _HomeState extends State<Home> {
         borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
         elevation: 8,
         items: <BubbleBottomBarItem>[
-            BubbleBottomBarItem(backgroundColor: Colors.black, icon: SvgPicture.asset('assets/icons/home.svg',width: 21,color: Colors.black54, height: 21,), activeIcon: SvgPicture.asset('assets/icons/home.svg',width: 21,color: Colors.black, height: 21,), title: Text("Home")),
-            BubbleBottomBarItem(backgroundColor: Colors.black, icon: SvgPicture.asset('assets/icons/search.svg',width: 21,color: Colors.black54, height: 21,), activeIcon: SvgPicture.asset('assets/icons/search.svg',width: 21,color: Colors.black, height: 21,), title: Text("Search")),
-            BubbleBottomBarItem(backgroundColor: Colors.black, icon: SvgPicture.asset('assets/icons/bookmark.svg',width: 21,color: Colors.black54, height: 21,), activeIcon: SvgPicture.asset('assets/icons/bookmark.svg',width: 21,color: Colors.black, height: 21,), title: Text("Bookmarks")),
-            BubbleBottomBarItem(backgroundColor: Colors.black, icon: Container(
-              height: 24,width: 24,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(image: AssetImage('assets/user.png')),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color(0x5c000000),
-                    offset: Offset(0,1),
-                    blurRadius: 5
-                  )
-                ],
+          BubbleBottomBarItem(
+              backgroundColor: Colors.black,
+              icon: SvgPicture.asset(
+                'assets/icons/home.svg',
+                width: 21,
+                color: Colors.black54,
+                height: 21,
               ),
-            ), title: Text("Profile")),
+              activeIcon: SvgPicture.asset(
+                'assets/icons/home.svg',
+                width: 21,
+                color: Colors.black,
+                height: 21,
+              ),
+              title: Text("Home")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.black,
+              icon: SvgPicture.asset(
+                'assets/icons/search.svg',
+                width: 21,
+                color: Colors.black54,
+                height: 21,
+              ),
+              activeIcon: SvgPicture.asset(
+                'assets/icons/search.svg',
+                width: 21,
+                color: Colors.black,
+                height: 21,
+              ),
+              title: Text("Search")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.black,
+              icon: SvgPicture.asset(
+                'assets/icons/bookmark.svg',
+                width: 21,
+                color: Colors.black54,
+                height: 21,
+              ),
+              activeIcon: SvgPicture.asset(
+                'assets/icons/bookmark.svg',
+                width: 21,
+                color: Colors.black,
+                height: 21,
+              ),
+              title: Text("Bookmarks")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.black,
+              icon: Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(image: AssetImage('assets/user.png')),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color(0x5c000000),
+                        offset: Offset(0, 1),
+                        blurRadius: 5)
+                  ],
+                ),
+              ),
+              title: Text("Profile")),
         ],
       ),
     );
