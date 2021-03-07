93adbb51b3b84b99fd4e08629037fa2dbdad2466
Maadhav Sharma <maadhav2001@gmail.com>
2021-03-07 20:16:01 +0530

    updated data in accordance to the new XML

--- lib/backend/rss_to_json.dart
+++ lib/backend/rss_to_json.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 import 'package:xml2json/xml2json.dart';
 import 'package:http/http.dart' as http;
 
-Future<List> rssToJson(String category, {String baseUrl = 'https://www.hindustantimes.com/rss/'}) async {
+Future<List> rssToJson(String category,
+    {String baseUrl = 'https://www.hindustantimes.com/feeds/rss/'}) async {
   var client = http.Client();
   final myTranformer = Xml2Json();
   return await client.get(baseUrl + category + '/rssfeed.xml').then((response) {
