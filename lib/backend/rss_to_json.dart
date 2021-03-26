4d959f9ef075ec2f7914cf628d2a8038209cb3ef
Maadhav Sharma <maadhav2001@gmail.com>
2021-03-26 21:16:22 +0530

    bug fixed

--- lib/backend/rss_to_json.dart
+++ lib/backend/rss_to_json.dart
@@ -7,11 +7,12 @@ Future<List> rssToJson(String category,
     {String baseUrl = 'https://www.hindustantimes.com/feeds/rss/'}) async {
   var client = http.Client();
   final myTranformer = Xml2Json();
-  return await client.get(baseUrl + category + '/rssfeed.xml').then((response) {
-    return response.body;
-  }).then((bodyString) {
-    myTranformer.parse(bodyString);
-    var json = myTranformer.toGData();
-    return jsonDecode(json)['rss']['channel']['item'];
-  });
+  var response = await client.get(baseUrl + category + '/rssfeed.xml');
+  myTranformer.parse(response.body);
+  var json = myTranformer.toGData();
+  var result = jsonDecode(json)['rss']['channel']['item'];
+  if (!(result is List<dynamic>)) {
+    return [result];
+  }
+  return result;
 }
